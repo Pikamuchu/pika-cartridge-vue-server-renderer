e7b120d19cc992d61f552526422e9ca498490019
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2021-02-13 09:58:06 +0100

    Adding basic  tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
+import Vue from 'vue';
 const sinon = require('sinon');
 const { assert } = require('chai');
 const searchquire = require('searchquire');
+
 
 describe('vue-server-renderer library', () => {
     let vueServerRenderer;
@@ -25,6 +27,79 @@ describe('vue-server-renderer library', () => {
     describe('vue-server-renderer initialization', () => {
         it('vue-server-renderer defined', () => {
             assert.isDefined(vueServerRenderer);
+        });
+
+        it('should work', done => {
+            vueServerRenderer(
+                new Vue({
+                    template: `
+                <div>
+                  <p class="hi">yoyo</p>
+                  <div id="ho" :class="{ red: isRed }"></div>
+                  <span>{{ test }}</span>
+                  <input :value="test">
+                  <img :src="imageUrl">
+                  <test></test>
+                  <test-async></test-async>
+                </div>
+              `,
+                    data: {
+                        test: 'hi',
+                        isRed: true,
+                        imageUrl: 'https://vuejs.org/images/logo.png'
+                    },
+                    components: {
+                        test: {
+                            render() {
+                                return this.$createElement('div', { class: ['a'] }, 'test');
+                            }
+                        },
+                        testAsync(resolve) {
+                            resolve({
+                                render() {
+                                    return this.$createElement('span', { class: ['b'] }, 'testAsync');
+                                }
+                            });
+                        }
+                    }
+                }),
+                (err, result) => {
+                    assert.isNull(err);
+                    assert.include(
+                        result,
+                        '<div data-server-rendered="true">' +
+                            '<p class="hi">yoyo</p> ' +
+                            '<div id="ho" class="red"></div> ' +
+                            '<span>hi</span> ' +
+                            '<input value="hi"> ' +
+                            '<img src="https://vuejs.org/images/logo.png"> ' +
+                            '<div class="a">test</div> ' +
+                            '<span class="b">testAsync</span>' +
+                            '</div>'
+                    );
+                    done();
+                }
+            );
+        });
+
+        // #5941
+        it('should work peoperly when accessing $ssrContext in root component', done => {
+            let ssrContext;
+            vueServerRenderer(
+                new Vue({
+                    template: `
+                <div></div>
+              `,
+                    created() {
+                        ssrContext = this.$ssrContext;
+                    }
+                }),
+                err => {
+                    assert.isNull(err);
+                    assert.isUndefined(ssrContext);
+                    done();
+                }
+            );
         });
     });
 });
